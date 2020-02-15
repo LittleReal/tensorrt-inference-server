7009d97e4d4daa4d78b326f5ab66faec63c81124
wzhirui <wzhirui@infervision.com>
2020-02-15 21:22:07 +0800

    add error

---
+++ src/custom/mxnet/error_codes.cc
@@ -0,0 +1,40 @@
+#include "src/custom/mxnet/error_codes.h"
+
+ErrorCodes::ErrorCodes()
+{
+  RegisterError(Success, "success");
+  RegisterError(CreationFailure, "failed to create instance");
+  RegisterError(InvalidModelConfig, "invalid model configuration");
+  RegisterError(
+      InvalidInvocationV1,
+      "invalid V1 function invocation while the custom backend is not V1");
+  RegisterError(
+      InvalidInvocationV2,
+      "invalid V2 function invocation while the custom backend is not V2");
+  RegisterError(Unknown, "unknown error");
+}
+
+const char*
+ErrorCodes::ErrorString(int error) const
+{
+  if (ErrorIsRegistered(error)) {
+    return err_messages_[error].c_str();
+  }
+
+  return err_messages_[Unknown].c_str();
+}
+
+int
+ErrorCodes::RegisterError(const string& error_string)
+{
+  err_messages_.push_back(error_string);
+  return static_cast<int>(err_messages_.size() - 1);
+}
+
+void
+ErrorCodes::RegisterError(int error, const string& error_string)
+{
+  if (ErrorIsRegistered(error)) {
+    err_messages_[error] = error_string;
+  }
+}
---
+++ src/custom/mxnet/instance.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "src/backends/custom/custom.h"
+#include "src/core/model_config.h"
+#include "src/core/model_config.pb.h"
+
+// Path for c_predict_api
+#include <mxnet/c_predict_api.h>
+
+class MxnetInstance{
+    public:
+
+        static int Create(
+        MxnetInstance** instance, const string& name,
+        const ModelConfig& model_config, int gpu_device,
+        const CustomInitializeData* data);
+
+        MxnetInstance(
+        const string& instance_name, const ModelConfig& model_config,
+        int gpu_device);
+
+        virtual ~MxnetInstance() = default;
+
+        virtual int ExecuteV1(
+            const uint32_t payload_cnt, CustomPayload* payloads,
+            CustomGetNextInputFn_t input_fn, CustomGetOutputFn_t output_fn)
+        {
+            return ErrorCodes::InvalidInvocationV1;
+        }
+
+        virtual int ExecuteV2(
+            const uint32_t payload_cnt, CustomPayload* payloads,
+            CustomGetNextInputV2Fn_t input_fn, CustomGetOutputV2Fn_t output_fn)
+        {
+            return ErrorCodes::InvalidInvocationV2;
+        }
+    
+
+    private:
+
+        PredictorHandle pred_hnd_;
+
+        const string instance_name_;
+        
+        const ModelConfig model_config_;
+
+        const int gpu_device_;
+        
+        ErrorCodes errors_{};
+}
